05532d2acd0cff028adc887470a4df48b778caf7
Mafaz-2112 <foozadaary2017@gmail.com>
2023-09-08 15:32:49 -0700

    ohoo

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Description: printf  alphabet in upper/lower case
+ *
+ * Return: always  0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n');
+
+	return (0);
+}
