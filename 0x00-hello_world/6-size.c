4f31995886274fb237c37aa96c835ff5011ef20a
Mafaz-2112 <foozadaary2017@gmail.com>
2023-09-07 18:35:17 -0700

    never give up

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - entary point
+ * main - Entary point
  *
- * descreption: using sizeof to print the size of various types
+ * Rescreption: using sizeof to print the size of various types
  *
- * return: aleays 0 (success)
+ * Return: aleays 0 (success)
 */
 
 int main(void)
 {
 	printf("size of a char: %u byte(s)\n", sizeof(char));
-	printf("size of a int: %u byte(s)\n", sizeof(int));
+	printf("size of an int: %u byte(s)\n", sizeof(int));
 	printf("size of a long int: %u byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %u byte(s)\n", sizeof(long long int));
 	printf("size of a float: %u byte(s)\n", sizeof(float));
