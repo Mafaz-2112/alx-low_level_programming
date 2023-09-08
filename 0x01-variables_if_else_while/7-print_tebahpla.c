3a744a41a2e040fc76be9eba54cd3290b2aebdc4
Mafaz-2112 <foozadaary2017@gmail.com>
2023-09-08 15:57:56 -0700

    h

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Description: print tebahpla
+ *
+ * Return: always  0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch <= 'a')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
