e3167d1993e93b8ce2039cc0cedfbed581666369
Mafaz-2112 <foozadaary2017@gmail.com>
2023-09-12 19:13:24 -0700

    lol

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Descreption : computes th sum of
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int sum, nim;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0))
+			sum += num;
+	}
+	printf("%d/n", sum);
+
+	return (0);
+}
